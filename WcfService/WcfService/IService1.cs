97fea7b2d7f46f34a188b6980e3f8d5e4344c9f3
arvinoth21 <arvinoth21@gmail.com>
2018-11-27 19:29:58 +0530

    WCF service

---
+++ WcfService/WcfService/IService1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string SayHello(string name);
+
+        [OperationContract]
+        string TodayProgram(string name);
+
+        [OperationContract]
+        List<string> OpeningJobs();
+
+        [OperationContract]
+        string OpeningJobsByRole(int id);
+
+        [OperationContract]
+        int Add(int a, int b);
+
+        [OperationContract]
+        int Subtract(int a, int b);
+        // TODO: Add your service operations here
+    }
+}
---
+++ WcfService/WcfService/Service1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        public string SayHello(string name)
+        {
+            string wish = "";
+            if (DateTime.Now.Hour < 12)
+            {
+                wish = "Good Morning";
+            }
+            else if (DateTime.Now.Hour < 17)
+            {
+                wish = "Good Afternoon";
+            }
+            else
+            {
+                wish = "Good Evening";
+            }
+
+            return "Hello  " + name + "    Wish You..  " + wish;
+
+        }
+
+        public string TodayProgram(string name)
+        {
+            if (DateTime.Now.DayOfWeek.ToString() == "Sunday" || DateTime.Now.DayOfWeek.ToString() == "Sataurday")
+            {
+                return "Hi-  " + name + "  Happy Weekend";
+            }
+            else
+            {
+                return "Hi-  " + name + "   Enjoy Workday";
+            }
+        }
+
+        public List<string> OpeningJobs()
+        {
+            List<string> objjob = new List<string>() { "IT", "Mechanic", "Civil", "Tailoring" };
+
+            return objjob;
+
+        }
+
+        public string OpeningJobsByRole(int id)
+        {
+            List<string> objjob = new List<string>() { "IT", "Mechanic", "Civil", "Tailoring" };
+
+            if (id <= 3)
+            {
+                return objjob[id];
+            }
+            else
+            {
+                return "Job not found";
+            }
+
+        }
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
